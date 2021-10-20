b05c9adf3cf22c3ad36b8828c19f06c4eb294d27
antongalushka <91842582+antongalushka@users.noreply.github.com>
2021-10-20 09:08:51 +0300

    Add files via upload

---
+++ newpro/js/script.js
@@ -0,0 +1,21 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+	
+});
+$(document).ready(function () {
+	$('.head__burger').click(function (event) {
+		$('.head__burger,.head__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
